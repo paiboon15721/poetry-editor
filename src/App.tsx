74b70697b371523f55e58bb2d71c59a40f7e5f11
paiboon-tw <paiboon.a@thoughtworks.com>
2021-12-28 21:25:02 +0700

    wireup with mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
-const Box: React.FC<{ x: string }> = ({ x }) => (
-  <input
-    style={{
-      width: '25px',
-      height: '25px',
-      textAlign: 'center',
-      textTransform: 'uppercase',
-    }}
-    onKeyDown={x => {
-      console.log('xx', x)
-    }}
-    maxLength={1}
-  />
-)
+import { observer, useLocalObservable } from 'mobx-react-lite'
+import { createRef, RefObject, useEffect, useRef } from 'react'
 
-const Grid: React.FC<{ xs: string[] }> = ({ xs }) => {
+const buildInitialValues = (n: number) => Array(n * n).fill('x')
+
+const Box: React.FC<{
+  childRef: RefObject<HTMLInputElement>
+  value: string
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+}> = observer(({ childRef, value, onChange }) => {
+  return (
+    <input
+      style={{
+        width: '25px',
+        height: '25px',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+      }}
+      ref={childRef}
+      value={value}
+      onKeyDown={x => {
+        console.log('keydown', x)
+      }}
+      onChange={onChange}
+      onFocus={() => {
+        childRef.current?.select()
+      }}
+      onClick={() => {
+        childRef.current?.select()
+      }}
+      maxLength={1}
+    />
+  )
+})
+
+const Grid: React.FC = observer(() => {
+  const xs = useLocalObservable(() => buildInitialValues(10))
+  const refs = useRef<RefObject<HTMLInputElement>[]>([])
+  useEffect(() => {
+    refs.current = refs.current.slice(0, xs.length)
+  }, [xs])
+
   return (
     <div style={{ overflow: 'auto', whiteSpace: 'nowrap' }}>
-      {xs.map((x, i) => (
-        <span key={i}>
-          <Box x="x" />
-          {(i + 1) % Math.sqrt(xs.length) === 0 && <br />}
-        </span>
-      ))}
+      {xs.map((x, i) => {
+        refs.current[i] = createRef()
+        return (
+          <span key={i}>
+            <Box
+              childRef={refs.current[i]}
+              value={x}
+              onChange={x => {
+                xs[i] = x.target.value
+              }}
+            />
+            {(i + 1) % Math.sqrt(xs.length) === 0 && <br />}
+          </span>
+        )
+      })}
     </div>
   )
-}
+})
 
 const App = () => {
-  const buildInitialValues = (n: number) => Array(n * n).fill('')
-  return <Grid xs={buildInitialValues(10)} />
+  return <Grid />
 }
 
 export default App
